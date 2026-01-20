c540d5bfdc55c9294e8b18a2f63f791b85c85fe6
Devgoodness <emmanuelgoodness923@gmail.com>
2026-01-20 04:24:00 -0800

    code update

--- cryptoflow.js
+++ cryptoflow.js
@@ -1,1 +1,22 @@
 
+fetch(
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin,solana&vs_currencies=usd&include_24hr_change=true"
+)
+  .then(response => response.json())
+  .then(data => {
+
+    setData("btc-price", "btc-change", data.bitcoin.usd, data.bitcoin.usd_24h_change);
+    setData("eth-price", "eth-change", data.ethereum.usd, data.ethereum.usd_24h_change);
+    setData("doge-price", "doge-change", data.dogecoin.usd, data.dogecoin.usd_24h_change);
+    setData("sol-price", "sol-change", data.solana.usd, data.solana.usd_24h_change);
+  });
+
+function setData(priceId, changeId, price, change) {
+  const priceEl = document.getElementById(priceId);
+  const changeEl = document.getElementById(changeId);
+
+  priceEl.textContent = price.toLocaleString();
+  changeEl.textContent = change.toFixed(2) + "%";
+
+  changeEl.style.color = change < 0 ? "red" : "green";
+}
