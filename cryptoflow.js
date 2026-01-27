e7cd3701b2a8fdcbc513c04eab0586bf96b2e59b
Devgoodness <emmanuelgoodness923@gmail.com>
2026-01-27 07:10:20 -0800

    updating api calls

--- cryptoflow.js
+++ cryptoflow.js
@@ -1,6 +1,6 @@
 
 fetch(
-  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin,solana&vs_currencies=usd&include_24hr_change=true"
+  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin,solana,usdt&vs_currencies=usd&include_24hr_change=true"
 )
   .then(response => response.json())
   .then(data => {
