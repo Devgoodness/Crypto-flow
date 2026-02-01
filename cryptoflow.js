17e606c18f71b610530b01da34a626895d9ae0b6
Devgoodness <emmanuelgoodness923@gmail.com>
2026-01-31 19:30:42 -0800

    updated js code

--- cryptoflow.js
+++ cryptoflow.js
@@ -5,10 +5,7 @@ fetch(
   .then(response => response.json())
   .then(data => {
 
-    setData("btc-price", "btc-change", data.bitcoin.usd, data.bitcoin.usd_24h_change);
-    setData("eth-price", "eth-change", data.ethereum.usd, data.ethereum.usd_24h_change);
-    setData("doge-price", "doge-change", data.dogecoin.usd, data.dogecoin.usd_24h_change);
-    setData("sol-price", "sol-change", data.solana.usd, data.solana.usd_24h_change);
+
   });
 
 function setData(priceId, changeId, price, change) {
